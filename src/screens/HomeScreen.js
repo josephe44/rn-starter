24deef85233316a04781e280041d7a5d5617f3cb
josephe44 <josephe442@gmail.com>
2023-04-06 11:11:44 +0100

    learnt about rn navigation and buttons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,50 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
-    <Text style={styles.text}>
-      Hi There
-    </Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hi There</Text>
+      <View style={styles.defaultButton}>
+        <Button
+          onPress={() => navigation.navigate("Components")}
+          title="Go to Components Demo"
+        />
+      </View>
+      <TouchableOpacity onPress={() => navigation.navigate("List")}>
+        <Text style={styles.styleButton}>Go to List Demo</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f8f8f8",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+  },
+
+  defaultButton: {
+    fontSize: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: "white",
+    marginBottom: 20,
+  },
+
   text: {
     fontSize: 30,
+    marginBottom: 20,
+  },
+  styleButton: {
+    fontSize: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: "red",
+    color: "white",
   },
 });
 
